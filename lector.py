985aeed0e31eb096cd315b88aef6e77da36bb311
Nicolas <nikolas.q@gmail.com>
2023-04-28 11:37:46 -0500

    cambios en el contador de las materias por colegio

--- lector.py
+++ lector.py
@@ -12,18 +12,24 @@ with open('titiribi.csv', encoding='latin-1') as file:
    cm = 0
    ce = 0
    ci = 0
+   nmca = 0
+   nmcb = 0
+   nmcc = 0
    cantidad = 0
    for row in reader:
     # separamos los colegios y almacenamos los datos de las notas
         if row[0] == 'A':
+            nmca += 1
             am += (int(row[1]))
             ae += (int(row[2]))
             ai += (int(row[3]))
         elif row[0] == 'B':
+            nmcb += 1
             bm += (int(row[1]))
             be += (int(row[2]))
             bi += (int(row[3]))
         elif row[0] == 'C':
+            nmcc += 1
             cm += (int(row[1]))
             ce += (int(row[2]))
             ci += (int(row[3]))
@@ -31,38 +37,42 @@ with open('titiribi.csv', encoding='latin-1') as file:
 
 cantidad = cantidad - 1
 
-def calculo(cantidad, nota):
-    return round(nota/cantidad, 2)
+print(f'El colegio A tiene {nmca} estudiantes')
+print(f'El colegio B tiene {nmcb} estudiantes')
+print(f'El colegio C tiene {nmcc} estudiantes')
+
+def calculo(c, nota):
+    return round(nota/c, 2)
 
 
 # mejor promedio en matematicas
-if calculo(cantidad, am) > calculo(cantidad, bm) and calculo(cantidad, am) > calculo(cantidad, cm):
-    print(f'El colegio A tiene el mejor promedio en matematicas {calculo(cantidad, am)}')
-elif calculo(cantidad, bm) > calculo(cantidad, am) and calculo(cantidad, bm) > calculo(cantidad, cm):
-    print(f'El colegio B tiene el mejor promedio en matematicas {calculo(cantidad, bm)}')
-elif calculo(cantidad, cm) > calculo(cantidad, am) and calculo(cantidad, cm) > calculo(cantidad, bm):
-    print(f'El colegio C tiene el mejor promedio en matematicas {calculo(cantidad, cm)}')
+if calculo(nmca, am) > calculo(nmca, bm) and calculo(nmca, am) > calculo(nmca, cm):
+    print(f'El colegio A tiene el mejor promedio en matematicas {calculo(nmca, am)}')
+elif calculo(nmca, bm) > calculo(nmca, am) and calculo(nmca, bm) > calculo(nmca, cm):
+    print(f'El colegio B tiene el mejor promedio en matematicas {calculo(nmca, bm)}')
+elif calculo(nmca, cm) > calculo(nmca, am) and calculo(nmca, cm) > calculo(nmca, bm):
+    print(f'El colegio C tiene el mejor promedio en matematicas {calculo(nmca, cm)}')
 else:
     print('Los colegios tienen el mismo promedio en matematicas')
 
 # mejor promedio en español
-if calculo(cantidad, ae) > calculo(cantidad, be) and calculo(cantidad, ae) > calculo(cantidad, ce):
-    print(f'El colegio A tiene el mejor promedio en español {calculo(cantidad, ae)}')
-elif calculo(cantidad, be) > calculo(cantidad, ae) and calculo(cantidad, be) > calculo(cantidad, ce):
-    print(f'El colegio B tiene el mejor promedio en español {calculo(cantidad, be)}')
-elif calculo(cantidad, ce) > calculo(cantidad, ae) and calculo(cantidad, ce) > calculo(cantidad, be):
-    print(f'El colegio C tiene el mejor promedio en español {calculo(cantidad, ce)}')
+if calculo(nmcb, ae) > calculo(nmcb, be) and calculo(nmcb, ae) > calculo(nmcb, ce):
+    print(f'El colegio A tiene el mejor promedio en español {calculo(nmcb, ae)}')
+elif calculo(nmcb, be) > calculo(nmcb, ae) and calculo(nmcb, be) > calculo(nmcb, ce):
+    print(f'El colegio B tiene el mejor promedio en español {calculo(nmcb, be)}')
+elif calculo(nmcb, ce) > calculo(nmcb, ae) and calculo(nmcb, ce) > calculo(nmcb, be):
+    print(f'El colegio C tiene el mejor promedio en español {calculo(nmcb, ce)}')
 else:
     print('Los colegios tienen el mismo promedio en español')
 
 
 # mejor promedio en ingles
-if calculo(cantidad, ai) > calculo(cantidad, bi) and calculo(cantidad, ai) > calculo(cantidad, ci):
-    print(f'El colegio A tiene el mejor promedio en ingles {calculo(cantidad, ai)}')
-elif calculo(cantidad, bi) > calculo(cantidad, ai) and calculo(cantidad, bi) > calculo(cantidad, ci):
-    print(f'El colegio B tiene el mejor promedio en ingles {calculo(cantidad, bi)}')
-elif calculo(cantidad, ci) > calculo(cantidad, ai) and calculo(cantidad, ci) > calculo(cantidad, bi):
-    print(f'El colegio C tiene el mejor promedio en ingles {calculo(cantidad, ci)}')
+if calculo(nmcc, ai) > calculo(nmcc, bi) and calculo(nmcc, ai) > calculo(nmcc, ci):
+    print(f'El colegio A tiene el mejor promedio en ingles {calculo(nmcc, ai)}')
+elif calculo(nmcc, bi) > calculo(nmcc, ai) and calculo(nmcc, bi) > calculo(nmcc, ci):
+    print(f'El colegio B tiene el mejor promedio en ingles {calculo(nmcc, bi)}')
+elif calculo(nmcc, ci) > calculo(nmcc, ai) and calculo(nmcc, ci) > calculo(nmcc, bi):
+    print(f'El colegio C tiene el mejor promedio en ingles {calculo(nmcc, ci)}')
 else:
     print('Los colegios tienen el mismo promedio en ingles')
 
